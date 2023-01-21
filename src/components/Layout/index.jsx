012ddf7b57bff85c66ff5136ab82339485d6cc9c
dilipdivate <dilip.divate@gmail.com>
2023-01-21 11:09:19 +1100

    Step3- Add Sidebar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -9,10 +9,13 @@ import Sidebar from "components/Layout/Sidebar";
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  // const { data } = useGetUserQuery(userId);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        // user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -20,6 +23,7 @@ const Layout = () => {
       />
       <Box flexGrow={1}>
         <Navbar
+          // user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
